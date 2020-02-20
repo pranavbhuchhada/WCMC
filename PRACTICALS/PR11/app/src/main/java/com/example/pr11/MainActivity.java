651e6c19959e25a0553c2e338e9903ba3955b83e
pranavbhuchhada <pranavbhuchhada@gmail.com>
2020-02-20 10:18:18 +0530

    Practical 10 Completed

--- PRACTICALS/PR11/app/src/main/java/com/example/pr11/MainActivity.java
+++ PRACTICALS/PR11/app/src/main/java/com/example/pr11/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -22,16 +23,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Configure sign-in to request the user's ID, email address, and basic
-// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
-                .requestIdToken("1067856233891-c33htq4nmo7cpc2pjfhm2gfhleicq6bs.apps.googleusercontent.com")
                 .build();
-        // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        // Set the dimensions of the sign-in button.
         SignInButton signInButton = findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_STANDARD);
 
@@ -43,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
                         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
                         startActivityForResult(signInIntent, 9411);
                         break;
-                    // ...
                 }
             }
         });
@@ -52,37 +46,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if ( account == null) return;
+        updateUI(account);
     }
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
             updateUI(account);
         } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.e("SSO","signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
         }
     }
-
-    private void updateUI(Object o) {
+    private void updateUI(GoogleSignInAccount account) {
+        Toast.makeText(this,"Welcome " + account.getDisplayName(),Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == 9411) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
-
     }
 }
